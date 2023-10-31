aa30ade145e7e611c594f999f9d0514e4ed39e2e
dude <dude>
2023-10-31 21:43:48 +0500

    combinatorics research

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-import sqlite3
-db = sqlite3.connect('DataBase')
-cur = db.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS table1 ')
+from multiset import FrozenMultiset as fms
+def nand(a, b):
+    return 0b11111111 ^ (a & b)
+
+
+def binary(a):
+    return bin(a)[3:]
+
+stack = [0b100001111, 0b100110011]
+set = set(stack)
+combinations = {}
+i = 0
+for x in stack:
+    print(binary(x), end=' ')
+    j = 0
+    for y in stack:
+        z = nand(x, y)
+        combinations[fms((x, y))] = combinations.get(fms((x, y)), 0) + 1
+        j += 1
+        if z not in set:
+            stack.append(z)
+            set.add(z)
+    i += j
+    print(j)
+print('total combinations', i)
+
+
+sum = 0
+counts = [0]
+for pair in combinations.items():
+    sum += pair[1]
+    print(pair[0], pair[1])
+
+print(len(combinations))
+#print(len(stack))
