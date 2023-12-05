989a3a36466ec60d1c586cebd78589207c840c21
dude <dude>
2023-12-05 22:17:09 +0500

    path finder

--- WebPathFinder/WikiParser.py
+++ WebPathFinder/WikiParser.py
@@ -31,4 +31,4 @@ def get_all_links(url):
 math_URL = "https://ru.wikipedia.org/wiki/%D0%9C%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0"
 pic_URL = "https://ru.wikipedia.org/wiki/%D0%A4%D0%B0%D0%B9%D0%BB:Caesar3.svg"
 links = get_all_links(math_URL)
-for url in links: webbrowser.open_new("https://ru.wikipedia.org/"+url)
+#for url in links: webbrowser.open_new("https://ru.wikipedia.org/"+url)
--- WebPathFinder/test.py
+++ WebPathFinder/test.py
@@ -1,5 +1,2 @@
-import requests
-import lxml
-import json
-response = lxml.html.pars("https://ru.wikipedia.org/wiki/%D0%9C%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0")
-print(response)
+a = {1:2, 3:4}
+print(2 in a)
