455cb2fd1ed6f1689c8500bb66cb3306a336f797
Jmasko <ch20dima@gmail.com>
2024-02-29 21:55:15 +0500

    sync previos course leftovers

--- WikiPathFinder/Main.py
+++ WikiPathFinder/Main.py
@@ -10,13 +10,18 @@ FormalSystem = "/wiki/%D0%A4%D0%BE%D1%80%D0%BC%D0%B0%D0%BB%D1%8C%D0%BD%D0%B0%D1%
 Philosofy = "/wiki/%D0%A4%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84%D0%B8%D1%8F"
 Logic = "/wiki/%D0%9B%D0%BE%D0%B3%D0%B8%D0%BA%D0%B0"
 Expirience = "/wiki/%D0%9E%D1%89%D1%83%D1%89%D0%B5%D0%BD%D0%B8%D0%B5"
+AC_DC = "/wiki/AC/DC"
 
 start = Math
-target = Expirience
+target = Logic
 links_cource = get_links()
-tree, dist = PathFinder.BFS(start, target, links_cource)
-path = PathFinder.TreeNodeToRootPath(tree, target)
-
-
-print(f"distance: {dist},  web requests made: {get_links.web_request_count},  cache hits: {get_links.cach_hit_count}")
-get_links.Batch_tab_open(path)
+try:
+    tree, dist = PathFinder.BFS(start, target, links_cource)
+    if dist!= None:
+        path = PathFinder.TreeNodeToRootPath(tree, target)
+        print(f"distance: {dist},  web requests made: {get_links.web_request_count},  cache hits: {get_links.cach_hit_count}")
+        get_links.Batch_tab_open(path)
+    else:
+        print(f"can't find target. web requests made: {get_links.web_request_count},  cache hits: {get_links.cach_hit_count}")
+except Exception:
+    print("some error. IDK bro")
