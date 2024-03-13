73787fe7789e4aa7e74e85806be6c170a0c067b8
Jmasko <ch20dima@gmail.com>
2024-03-13 23:11:32 +0500

    hex dump

--- Network/Cherniavsky_Hex_Dump.py
+++ Network/Cherniavsky_Hex_Dump.py
@@ -1,5 +1,7 @@
 Bytes = b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456/'
-line_len = 8
+#specify number of bytes per dump line
+
+line_len = 16
 Dump = '\n'.join([f'{i:05x}:' + ''.join([ f' {Byte:2x}' for Byte in Bytes[i:i+line_len]])  for i in range(0,len(Bytes), line_len)])
 
 print(Dump)
