d40b0bb4df500aeeeeddb23a940530cf49d75ef9
Jmasko <ch20dima@gmail.com>
2024-04-02 02:02:03 +0500

    quaternions cube rotation

--- 1.py
+++ 1.py
@@ -1,3 +1,16 @@
-arr = [('a', '1'), ('b', '2'), ('c', '3')]
+def f(x,y,n, N):
+    #print(" | "*n, x, y)
+    if n == N: return 1
+    s = 0
 
-print('{0} {1}'.format(i, *map(''.join, zip(*arr))))
+    for i in range(x + 1, y + 1):
+        s += f(i, y, n+1, N)
+    
+    for j in range(y+1, n):
+        for i in range(j + 1):
+            s += f(i, j, n+1, N)
+    return s
+
+
+#print(f(0, -1, 2, 5))
+for N in range(2, 12):print(N, f(0, -1, 2, N))
