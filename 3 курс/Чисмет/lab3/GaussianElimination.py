b9054809bb7bb3aae8a483dc0daa6c22d171e856
Jmasko <ch20dima@gmail.com>
2024-12-18 09:51:12 +0500

    optimization part1

--- 3 курс/Чисмет/lab3/GaussianElimination.py
+++ 3 курс/Чисмет/lab3/GaussianElimination.py
@@ -23,8 +23,8 @@ class GaussianElimination():
         self.data = source
         self.correct = correct
         self.chose = chose
-        self.row_map = list(range(hight))
-        self.col_map = list(range(width))
+        self.row = list(range(hight))
+        self.col = list(range(width))
 
 
     def forward(self):
@@ -35,48 +35,58 @@ class GaussianElimination():
             if self.chose:
                 self.print_state()
                 # Find the maximum element in the remaining submatrix
-                max_element = abs(self.data[self.row_map[diag]][self.col_map[diag]])
-                max_row = diag
-                max_col = diag
+                max_element = abs(self.data[self.row[diag]][self.col[diag]])
+                i_max = diag
+                j_max = diag
                 for i in range(diag, self.h):
+                    cur_row = self.row[i]
                     for j in range(diag, self.h):
-                        if abs(self.data[self.row_map[i]][self.col_map[j]]) > max_element:
-                            max_element = abs(self.data[self.row_map[i]][self.col_map[j]])
-                            max_row = i
-                            max_col = j
+                        cur_col = self.col[j]
+                        if (el := abs(self.data[cur_row][cur_col]) > max_element):
+                            max_element = el
+                            i_max = i
+                            j_max = j
+                            
                 
-                # Update row and column maps
-                self.row_map[diag], self.row_map[max_row] = self.row_map[max_row], self.row_map[diag]
-                self.col_map[diag], self.col_map[max_col] = self.col_map[max_col], self.col_map[diag]
-                print(f"max el {max_row + 1},{max_col + 1} is swaped")
+                # swap cur diaganal element with max via row, col permutation (maps values swaps)
+                
+                main_row = self.row[i_max]
+                main_col = self.col[j_max]
+                self.row[i_max] = self.row[diag]
+                self.col[j_max] = self.col[diag]
+                self.row[diag] = main_row
+                self.col[diag] = main_col
+                print(f"max el {i_max + 1},{j_max + 1} is swaped")
                 self.print_state()
 
             # Column calculations b_ij
             j = diag
             for i in range(diag, self.h):
-                exp = self.sub((self.data[self.row_map[i]][self.col_map[k]] * self.data[self.row_map[k]][self.col_map[j]] for k in range(j)), start=self.data[self.row_map[i]][self.col_map[j]])
+                cur_row = self.row[i]
+                exp = self.sub((self.data[cur_row][self.col[k]] * self.data[self.row[k]][main_col] for k in range(j)), start=self.data[cur_row][main_col])
                 print(f'b_{i + 1}{j + 1} = {exp.steps()}')
                 exp.flush()
-                self.data[self.row_map[i]][self.col_map[j]] = exp
+                self.data[cur_row][main_col] = exp
 
             # Row calculations c_ij
             i = diag
             for j in range(diag + 1, self.w):
-                exp = self.sub((self.data[self.row_map[i]][self.col_map[k]] * self.data[self.row_map[k]][self.col_map[j]] for k in range(i)), start=self.data[self.row_map[i]][self.col_map[j]])
-                exp = exp / self.data[self.row_map[i]][self.col_map[i]]
+                cur_col = self.col[j]
+                exp = self.sub((self.data[main_row][self.col[k]] * self.data[self.row[k]][cur_col] for k in range(i)), start=self.data[main_row][cur_col])
+                exp = exp / self.data[self.row[i]][self.col[i]]
                 print(f'c_{i + 1}{j + 1} = {exp.steps()}')
                 exp.flush()
-                self.data[self.row_map[i]][self.col_map[j]] = exp
+                self.data[main_row][cur_col] = exp
 
     def backward(self):
-        print("\nbackward step\n")
+        print("\nbackward ste p\n")
         self.print_state()
         last_col = self.w - 1
         for i in range(self.h-1, -1, -1):
-            exp = self.sub((self.data[self.row_map[i]][self.col_map[j]] * self.data[self.row_map[j]][self.col_map[last_col]] for j in range(self.h-1, i, -1)), start=self.data[self.row_map[i]][self.col_map[last_col]])
+            exp = self.sub((self.data[self.row[i]][self.col[j]] * self.data[self.row[j]][self.col[last_col]] for j in range(self.h-1, i, -1)), start=self.data[self.row[i]][self.col[last_col]])
             print(f"x'_{i + 1} = {exp.steps()}")
             exp.flush()
-            self.data[self.row_map[i]][self.col_map[last_col]] = exp
+            self.data[self.row[i]][self.col[last_col]] = exp
         self.print_state()
     
     def answer(self):
@@ -88,7 +98,7 @@ class GaussianElimination():
                 # Да-да двойное отобрпжение через таблицы перестановок.
                 # Почему? Я не имею ни малейшего понятия.
                 # Как я это вывел? Больно... очень больно.
-                x = self.data[self.row_map[self.col_map[i]]][self.col_map[j]]
+                x = self.data[self.row[self.col[i]]][self.col[j]]
                 vect.append(x)
                 print(x, end = '\t')
             ans.append(vect)
@@ -121,7 +131,7 @@ class GaussianElimination():
         for i in range(self.h):
             print('|' , end = '')
             for j in range(self.w):
-                print(self.data[self.row_map[i]][self.col_map[j]], end = '\t')
+                print(self.data[self.row[i]][self.col[j]], end = '\t')
             print('|')
         print()
 
